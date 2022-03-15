40876df7140d6c2ad8d9dd377929b6dd10d8ac6e
hiwiA <hiwotameha@gmail.com>
2022-03-15 14:08:52 -0700

    task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - add two ints
+ * @i: first int to add
+ * @j: second int to add
+ * Return: sum of both ints
+ **/
+
+int add(int i, int j)
+{
+		return (i + j);
+}
