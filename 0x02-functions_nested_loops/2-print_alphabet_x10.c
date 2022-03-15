42ec29189dff618d45e0cab338dda24500fc7814
hiwiA <hiwotameha@gmail.com>
2022-03-15 13:40:48 -0700

    task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 -prints 10 times the alphabet,in lower case,
+ *followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int a = 0;
+
+	while (a < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
