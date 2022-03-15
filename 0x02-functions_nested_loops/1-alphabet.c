0b302552f9b39e002989c4dadf3f494e883e13e6
hiwiA <hiwotameha@gmail.com>
2022-03-15 13:35:16 -0700

    task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet in lowercase.
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
