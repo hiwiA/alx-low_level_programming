353f1e73f34ded337e235b5b5e89757a67b0787e
Hiwot Ameha <hiwotameha23@gmail.com>
2022-04-04 23:27:02 +0300

    t5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,18 +14,18 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	size = 0;
-/*count the number
+/**count the number
  * of chars in each string 
- */	for (i = 0; i < ac; i++)
+ **/	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			size++;
 		size++;
 	}
 	size++;
-/* allocate memory for total number of chars and
+/** allocate memory for total number of chars and
  * new line for each word
- */
+ **/
 	strDup = malloc(sizeof(char) * size);
 	if (strDup == NULL)
 		return (NULL);
