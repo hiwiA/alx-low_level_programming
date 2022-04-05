00c8fba83eef276ed08895b05d0a612315341883
hiwiA <hiwotameha@gmail.com>
2022-04-04 23:03:12 -0700

    task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * *str_concat - concatenates two strings
+ *str_concat - concatenates two strings
  * @s1: first string
  * @s2: second string
  * Return: pointer to new space in memory or null
@@ -10,6 +10,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *strDup;
 	int i, j;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
